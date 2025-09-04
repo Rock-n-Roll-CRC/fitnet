d9c68ee471d81baac2d0de9789dffd10b9b6652c
Danil Dikhtyar <danil.dikhtyar@gmail.com>
2025-09-04 11:57:40 +0700

    🚧 Work on adding loading screen on navigation part 2

--- src/app/(app)/loading.tsx
+++ src/app/(app)/loading.tsx
@@ -3,5 +3,9 @@ import LoadingSpinnerSVG from "@/assets/icons/6-dots-scale.svg";
 import styles from "./loading.module.scss";
 
 export default function Loading() {
-  return <LoadingSpinnerSVG className={styles.loading} />;
+  return (
+    <main className={styles.main}>
+      <LoadingSpinnerSVG className={styles.loading} />
+    </main>
+  );
 }
